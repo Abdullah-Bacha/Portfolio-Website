bea39cc54ad96bdba1ac9f9ce08ce682c81a0e4b
Abdullah <bacha141998@gmail.com>
2026-02-13 18:18:44 +0500

    Delete News-App directory

--- News-App/src/Components/Wraper.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Wraper({children}) {
-  return (
-    <div className='max-w-[75rem] m-auto'>{children}</div>
-  )
-}
-
-export default Wraper
--- News-App/src/Config/axios.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-const api = axios.create({
-    baseURL: "https://newsapi.org/v2",
-    timeout: 15000,
-});
-
-
-export default api;
